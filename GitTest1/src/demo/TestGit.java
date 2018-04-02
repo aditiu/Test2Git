a617e920bff8d981d9f16b42f522addf795bb098
aditiu <aditi.18.coool@gmail.com>
2018-04-02 13:38:21 +0530

    Initial checkin test1

---
+++ GitTest1/src/demo/TestGit.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+	System.out.println("Created by aditi for test purpose");
+	System.out.println("This is for git eclipse jenkin test");
+
+	}
+
+}
