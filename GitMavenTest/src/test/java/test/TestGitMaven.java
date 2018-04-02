e6c3501b0430d0de5f6e8deffefcfc0d59dc1161
aditiu <aditi.18.coool@gmail.com>
2018-04-02 17:14:09 +0530

    This is first version

---
+++ GitMavenTest/src/test/java/test/TestGitMaven.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGitMaven {
+
+	@Test
+	public void pass() {
+		String s = "Aditi";
+		boolean flag = false;
+		if(s.length()>3){
+			System.out.println("Length is greater then three");
+			flag = true;
+		}
+		assertTrue(flag);
+	}
+	/*@Test
+	public void fail() {
+		String s = "Ad";
+		boolean flag = false;
+		if(s.length()>3){
+			System.out.println("Length is greater then three");
+			flag = true;
+		}
+		assertTrue(flag);
+	}*/
+}
